d9500ccf89074b03b6437f9b4eaa5aa4e54e21b0
KevinRaphaelMachado <machadokevin100@gmail.com>
2024-07-22 21:53:58 -0300

    primeiro commit

---
+++ dia.js
@@ -0,0 +1,55 @@
+const semana = []
+
+const bloco1 = function dias (ferias) {
+    
+    switch (semana) {
+        case "dia1":
+            console.log("o que vc fez no primeiro dia? /n");
+            break;
+            
+        case "dia2":
+            console.log("o que vc fez no segundo  dia? /n");
+            break;
+
+        case "dia3":
+            console.log("o que vc fez no terceiro dia? /n");
+            break;
+
+        case "dia4":
+            console.log("o que vc fez no quarto dia? /n");
+            break;
+
+        case "dia5":
+                console.log("o que vc fez no quinto dia? /n");
+                break;
+
+        default:
+            console.log("resposta invalida");
+            break;
+    }
+}
+
+        function criar (ferias) {
+        console.log("qual dia da semana iremos fala? /n 'dia1' 'dia2' 'dia3' 'dia4' ou  'dia5'? ");
+
+    }
+
+        function atualizar(ferias) {
+            console.log("qual dia você quer atualizar");
+        
+            if (semana)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    
+
+
